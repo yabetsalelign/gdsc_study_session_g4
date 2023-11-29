cac05f7a7445d6b8e43c23cbd477b7d7edbb46de
yabetsalelign <149236920+yabetsalelign@users.noreply.github.com>
2023-11-29 11:01:06 +0300

    Add files via upload

---
+++ BMI calculator.js
@@ -0,0 +1,4 @@
+let mass = parseFloat(prompt("please enter your mass in kilograms: "));
+let height = parseFloat(prompt("please enter your height in meters: "));  
+let bmi = mass / (height * height);
+console.log("Your BMI is:", bmi);
